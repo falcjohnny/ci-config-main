e1fb366747723bf60fb2d70a8f7c901220e59697
falcjohnny <johnny.wu@falconstor.com>
2015-12-10 15:29:17 +0800

    Update set_iscsi_name.sh

--- nodepool/scripts/set_iscsi_name.sh
+++ nodepool/scripts/set_iscsi_name.sh
@@ -14,7 +14,7 @@ echo $HOSTNAME > /tmp/image-hostname.txt
 sudo mv /tmp/image-hostname.txt /etc/image-hostname.txt
 
 # Quick fix so that slave node can resolve the master
-echo "192.168.2.32 ci-master" | sudo tee -a /etc/hosts
+echo "172.22.5.136 ci-master" | sudo tee -a /etc/hosts
 
 # Set iSCSI initiator name to make it unique
 INITNAME=`sudo iscsi-iname`
